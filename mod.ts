f819fb132ac18877ef06ba0b52ad3620c09a2755
Gnlow <gnlow@pm.me>
2025-10-22 01:06:22 +0900

    refactor use adt helper

--- mod.ts
+++ mod.ts
@@ -1,34 +1,47 @@
 type Expr =
     | { type: "Empty" }
-    | { type: "Epsilon"}
+    | { type: "Epsilon" }
     | { type: "Char", char: string }
     | { type: "Or", left: Expr, right: Expr }
     | { type: "Concat", left: Expr, right: Expr }
     | { type: "Star", arg: Expr }
 
+const Empty = { type: "Empty" } as const
+const Epsilon = { type: "Epsilon" } as const
+const Char =
+(char: string) =>
+    ({ type: "Char", char }) as const
+const Or =
+(left: Expr, right: Expr) =>
+    ({ type: "Or", left, right }) as const
+const Concat =
+(left: Expr, right: Expr) =>
+    ({ type: "Concat", left, right }) as const
+const Star =
+(arg: Expr) =>
+    ({ type: "Star", arg }) as const
+
 const nu =
 (expr: Expr): Expr => {
     switch (expr.type) {
         case "Empty":
-            return { type: "Empty" }
+            return Empty
         case "Epsilon":
-            return { type: "Epsilon" }
+            return Epsilon
         case "Char":
-            return { type: "Empty" }
+            return Empty
         case "Or":
-            return {
-                type: "Or",
-                left: nu(expr.left),
-                right: nu(expr.right),
-            }
+            return Or(
+                nu(expr.left),
+                nu(expr.right),
+            )
         case "Concat":
-            return {
-                type: "Concat",
-                left: nu(expr.left),
-                right: nu(expr.right),
-            }
+            return Concat(
+                nu(expr.left),
+                nu(expr.right),
+            )
         case "Star":
-            return { type: "Epsilon" }
+            return Epsilon
     }
 }
 
@@ -37,62 +50,45 @@ const deriv =
 (expr: Expr): Expr => {
     switch (expr.type) {
         case "Empty":
-            return { type: "Empty" }
+            return Empty
         case "Epsilon":
-            return { type: "Empty" }
+            return Empty
         case "Char":
             return expr.char == a
-                ? { type: "Epsilon" }
-                : { type: "Empty" }
+                ? Epsilon
+                : Empty
         case "Or":
-            return {
-                type: "Or",
-                left: deriv(a)(expr.left),
-                right: deriv(a)(expr.right),
-            }
+            return Or(
+                deriv(a)(expr.left),
+                deriv(a)(expr.right),
+            )
         case "Concat":
-            return {
-                type: "Or",
-                left: {
-                    type: "Concat",
-                    left: deriv(a)(expr.left),
-                    right: expr.right,
-                },
-                right: {
-                    type: "Concat",
-                    left: nu(expr.left),
-                    right: deriv(a)(expr.right),
-                },
-            }
+            return Or(
+                Concat(
+                    deriv(a)(expr.left),
+                    expr.right,
+                ),
+                Concat(
+                    nu(expr.left),
+                    deriv(a)(expr.right),
+                ),
+            )
         case "Star":
-            return {
-                type: "Concat",
-                left: deriv(a)(expr.arg),
-                right: expr,
-            }
+            return Concat(
+                deriv(a)(expr.arg),
+                expr,
+            )
     }
 }
 
 console.log(
-    deriv("a")({
-        type: "Concat",
-        left: {
-            type: "Or",
-            left: {
-                type: "Char",
-                char: "a",
-            },
-            right: {
-                type: "Char",
-                char: "b",
-            },
-        },
-        right: {
-            type: "Star",
-            arg: {
-                type: "Char",
-                char: "c",
-            },
-        },
-    })
+    deriv("a")(Concat(
+        Or(
+            Char("a"),
+            Char("b"),
+        ),
+        Star(
+            Char("c"),
+        ),
+    ))
 )
